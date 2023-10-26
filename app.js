2d3b290642d8e50d0d2be6c548d97a8f1f6fb4c5
José Cardoso <115185989+cardosojse@users.noreply.github.com>
2023-10-26 15:11:00 -0300

    new file

--- app.js
+++ app.js
@@ -19,3 +19,42 @@
 //         $(this).parent("li").fadeOut(200);
 //     });
 // });
+const taskInput = document.querySelector(".input__box");
+const taskBox = document.querySelector(".task__box");
+
+let todo = JSON.parse(localStorage.getItem("todo-list"));
+
+let showTodo = () => {
+    let li = "";
+    todo.forEach((todo, id) => {
+        li += `
+            <li class="list__box">
+                <label for="${id}">
+                    <input type="checkbox" id="${id}">
+                </label>
+                <p>${todo.name}</p>
+                <div class="settings">
+                    <img width="18" height="18" src="https://img.icons8.com/ios-glyphs/30/ellipsis.png" alt="ellipsis"/>
+                    <ul class="task__menu">
+                        <li><img src="img/edit.png" alt="edit">Edit</li>
+                        <li><img src="img/delete.png" alt="delete">Delete</li>
+                    </ul>
+                </div>
+            </li>
+        `
+    });
+    taskBox.innerHTML = li;
+}
+
+taskInput.addEventListener("keyup", (e) => {
+    let  userTask = taskInput.value.trim();
+    if (e.key == "Enter" && userTask){
+        if(!todo)
+            todo = [];
+        taskInput.value = "";
+        let taskInfo = {name: userTask, status: "pending"}
+        todo.push(taskInfo);
+        localStorage.setItem("todo-list", JSON.stringify(todo))
+        showTodo();
+    }  
+});
